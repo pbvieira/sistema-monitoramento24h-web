7dd4cdb8a3cf2b12321ddd1282ebadd3592fb7b9
DevNathanViana <oliveiranathan700@gmail.com>
2024-09-05 23:00:44 -0300

    Correção erro de Cors Policy

--- src/main/java/br/com/azindustria/azsim/config/CorsConfiguration.java
+++ src/main/java/br/com/azindustria/azsim/config/CorsConfiguration.java
@@ -5,10 +5,12 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig implements WebMvcConfigurer {
+public class CorsConfiguration implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173/", "http://localhost:3000/")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS",  "HEAD", "TRACE", "CONNECT");
     }
 }
